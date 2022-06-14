14542b9feb86cfbeeb0d2f2705016eccdfdfa942
Michael MacKinnon <michaelmackinnon@Michaels-MacBook-Pro.local>
2022-06-14 12:40:02 -0400

    adding stuff

---
+++ .lift/tool.sh
@@ -0,0 +1,33 @@
+
+
+
+#!/usr/bin/env bash
+dir=$1
+commit=$2
+cmd=$3 
+
+function version() {
+    echo 1
+}
+
+function applicable() {
+    echo "true"
+}
+
+function run() {
+    echo "[{ \"type\": \"Hello Friend - that's lame, maybe we should give you a name?\", \
+            \"message\": \"Lift is analyzing commit $commit\", \
+            \"file\": \"file.txt\", \
+            \"details_url\": \"https://example.com/#example\" \
+          }]"
+}
+
+if [[ "$cmd" = "run" ]] ; then 
+    run 
+fi 
+if [[ "$cmd" = "applicable" ]] ; then 
+    applicable 
+fi 
+if [[ "$cmd" = "version" ]] ; then 
+    version 
+fi
